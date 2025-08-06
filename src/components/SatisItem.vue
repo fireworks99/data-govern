5b6db873135033d2ae42b4059265f880c98f2971
fireworks99 <2639237361@qq.com>
2025-08-06 15:46:06 +0800

    统计组件

---
+++ src/components/SatisItem.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="satis_item">
+    <div class="left flex_center">
+      <div class="circle flex_center" :style="{ backgroundColor: color }">
+        <i :class="icon"></i>
+      </div>
+    </div>
+    <div class="right">
+      <div class="title">{{ title }}</div>
+      <div class="num">{{ num }}</div>
+    </div>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'SatisItem',
+  props: {
+    color: {
+      type: String,
+      default: '#10b981'
+    },
+    icon: {
+      type: String,
+      default: 'el-icon-circle-check'
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    num: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.satis_item {
+  width: 100%;
+  height: 64px;
+  background-color: #f8fafc;
+  border-radius: 8px;
+  display: flex;
+
+  .left {
+    width: 64px;
+
+    .circle {
+      width: 40px;
+      height: 40px;
+      border-radius: 20px;
+      color: #fff;
+    }
+  }
+  .right {
+    flex: 1;
+    padding: 12px 0;
+
+    .title {
+      color: #6e7786;
+      font-size: 14px;
+    }
+
+    .num {
+      line-height: 20px;
+      font-weight: bold;
+    }
+  }
+}
+</style>
--- src/views/govern/GovernStatus.vue
+++ src/views/govern/GovernStatus.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="wrapper">
     <div class="top">
-      <PanelLayout title="数据治理状态" icon="line"></PanelLayout>
+      <PanelLayout title="数据治理状态" icon="line">
+        <div class="intro">
+          当前流数据治理节点：
+          <span style="color: #10b981;font-weight: bold;">5</span>
+          , 其中异常节点：
+          <span style="color: #ef4444;font-weight: bold;">0</span>
+        </div>
+
+        <div class="row">
+          <SatisItem color="#10b981" icon="el-icon-circle-check" title="已完成文件" :num="2000"/>
+          <SatisItem color="#6b7280" icon="el-icon-time" title="未完成文件" :num="1000"/>
+          <SatisItem color="#ef4444" icon="el-icon-warning" title="待核查文件" :num="100"/>
+          <SatisItem color="#3b82f6" icon="el-icon-folder-opened" title="文件总数" :num="3100"/>
+        </div>
+      </PanelLayout>
     </div>
     <div class="bottom">
       <PanelLayout title="待核查文件列表" icon="warning"></PanelLayout>
@@ -11,11 +25,13 @@
 
 <script>
 import PanelLayout from '@/components/PanelLayout.vue';
+import SatisItem from '@/components/SatisItem.vue';
 
 export default {
   name: 'DataStatus',
   components: {
-    PanelLayout
+    PanelLayout,
+    SatisItem
   }
 }
 </script>
@@ -30,11 +46,25 @@ export default {
   gap: 16px;
 
   .top {
-    flex: 1;
+
+    .intro {
+      background-color: #f8fafc;
+      padding: 8px 12px;
+      color: #6e7786;
+      font-size: 14px;
+      text-align: center;
+    }
+
+    .row {
+      padding: 16px;
+      display: flex;
+      gap: 32px;
+    }
   }
 
   .bottom {
-    flex: 2;
+    flex: 1;
+    min-height: 0;
   }
 }
 </style>
